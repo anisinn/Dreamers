389bfdaad28be081cb0b1c003819ba4e64f822e9
Ani Sinnarkar <anisinn@hotmail.com>
2017-09-16 23:38:03 -0700

    Remove Line 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,11 @@ public class HelloWorld
 	{
 		System.out.println("Hello World Java !!") ;
 		System.out.println("Welcome to World Java Aditya !!") ;
+<<<<<<< HEAD
 		System.out.println("Additional Line !!") ;
 		System.out.println("Additional Line 2") ;
+=======
+>>>>>>> parent of b28d539... New Line
 
 	}
 }
