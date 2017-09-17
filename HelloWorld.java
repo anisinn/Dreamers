fbdca32b16824c724afce686d427231485417a26
Ani Sinnarkar <anisinn@hotmail.com>
2017-09-16 22:57:13 -0700

    Hello World Java program
    Hello World Java program

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World Java !!") ;
+		System.out.println("Welcome to World Java Aditya !!") ;
+
+	}
+}
